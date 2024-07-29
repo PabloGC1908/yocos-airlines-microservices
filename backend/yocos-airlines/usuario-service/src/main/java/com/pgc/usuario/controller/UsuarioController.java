77916e5d41ca47e1e8b4858ba2d8e77cc17db308
Pablo Guerra Camana <pguerracamana@gmail.com>
2024-07-29 01:12:56 -0500

    Agregando Mapper de Form de usuario a Usuario final

--- backend/yocos-airlines/usuario-service/src/main/java/com/pgc/usuario/controller/UsuarioController.java
+++ backend/yocos-airlines/usuario-service/src/main/java/com/pgc/usuario/controller/UsuarioController.java
@@ -1,39 +1,18 @@
 package com.pgc.usuario.controller;
 
-import com.pgc.usuario.dto.request.UsuarioFormRequest;
-import com.pgc.usuario.model.Usuario;
 import com.pgc.usuario.service.UsuarioService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/usuarios")
 @Slf4j
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
-    }
-
-    // TODO
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUsuario(@RequestBody UsuarioFormRequest usuario) {
-        log.info("Registrando usuario: {}", usuario);
-
-        boolean isUsuarioCreated = usuarioService.registerUsuario(usuario);
-
-        if (isUsuarioCreated)
-            return ResponseEntity.ok().build();
-        else
-            return ResponseEntity.badRequest().build();
-    }
-
-    // TODO
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUsuario(Usuario usuario) {
-        return null;
     }
 
     // TODO
