78ee088f35ad1073fd261ea8f9e2f35ba4bfd1e0
Pablo Guerra Camana <pguerracamana@gmail.com>
2024-08-13 16:20:15 -0500

    Terminado endpoint de creacion de Aeropuerto

--- backend/yocos-airlines/vuelo-service/src/main/java/com/pgc/vuelo/controller/AeropuertoController.java
+++ backend/yocos-airlines/vuelo-service/src/main/java/com/pgc/vuelo/controller/AeropuertoController.java
@@ -1,23 +1,34 @@
 package com.pgc.vuelo.controller;
 
+import com.pgc.vuelo.dto.request.AeropuertoRequest;
 import com.pgc.vuelo.models.Aeropuerto;
+import com.pgc.vuelo.service.AeropuertoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/airports")
 public class AeropuertoController {
+    private final AeropuertoService aeropuertoService;
+
+    public AeropuertoController(AeropuertoService aeropuertoService) {
+        this.aeropuertoService = aeropuertoService;
+    }
 
 
 
     // TODO: Implementar endpoint POST /airports.
-    public ResponseEntity<?> createAeropuerto(Aeropuerto aeropuerto) {
-        return null;
+    @PostMapping
+    public ResponseEntity<String> createAeropuerto(AeropuertoRequest aeropuertoRequest) {
+        String response = aeropuertoService.registerAeropuerto(aeropuertoRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     // TODO: Implementar endpoint DELETE /airports/{airportId}.
-    public ResponseEntity<?> deleteAeropuerto(Aeropuerto aeropuerto) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAeropuerto(@PathVariable("id") Long id) {
         return null;
     }
 }
--- backend/yocos-airlines/vuelo-service/src/main/java/com/pgc/vuelo/service/AeropuertoService.java
+++ backend/yocos-airlines/vuelo-service/src/main/java/com/pgc/vuelo/service/AeropuertoService.java
@@ -1,17 +1,24 @@
 package com.pgc.vuelo.service;
 
+import com.pgc.vuelo.dto.request.AeropuertoRequest;
 import com.pgc.vuelo.exceptions.classes.AeropuertoNotFoundException;
 import com.pgc.vuelo.models.Aeropuerto;
+import com.pgc.vuelo.models.Ciudad;
 import com.pgc.vuelo.repository.AeropuertoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 public class AeropuertoService {
+    private final CiudadService ciudadService;
+
     private final AeropuertoRepository aeropuertoRepository;
 
-    public AeropuertoService(AeropuertoRepository aeropuertoRepository) {
+    public AeropuertoService(CiudadService ciudadService,
+                             AeropuertoRepository aeropuertoRepository) {
+        this.ciudadService = ciudadService;
         this.aeropuertoRepository = aeropuertoRepository;
     }
 
@@ -19,5 +26,22 @@ public class AeropuertoService {
         Optional<Aeropuerto> aeropuerto = aeropuertoRepository.findById(id);
 
         return aeropuerto.orElseThrow(() -> new AeropuertoNotFoundException("Aeropuerto con id: " + id + " no existe"));
+    }
+
+    @Transactional
+    public String registerAeropuerto(AeropuertoRequest aeropuertoRequest) {
+        Ciudad ciudad = ciudadService.findCiudadById(aeropuertoRequest.idCiudad());
+
+        Aeropuerto aeropuerto = Aeropuerto.builder()
+                .aeropuerto(aeropuertoRequest.nombreAeropuerto())
+                .ciudad(ciudad)
+                .x(aeropuertoRequest.x())
+                .y(aeropuertoRequest.y())
+                .zonaHoraria(aeropuertoRequest.zonaHoraria())
+                .build();
+
+        aeropuertoRepository.save(aeropuerto);
+
+        return "Se registro el aeropuerto exitosamente";
     }
 }
