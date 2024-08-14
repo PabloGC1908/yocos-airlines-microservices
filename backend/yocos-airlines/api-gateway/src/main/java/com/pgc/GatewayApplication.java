dfe0a3ddd1e17adf5695088b69eabd1eadbe8768
Pablo Guerra Camana <pguerracamana@gmail.com>
2024-08-14 13:37:11 -0500

    Enlazando los servicios de usuario y vuelo al configserver y al api gateway

--- backend/yocos-airlines/api-gateway/src/main/java/com/pgc/GatewayApplication.java
+++ backend/yocos-airlines/api-gateway/src/main/java/com/pgc/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.pgc.vuelo;
+package com.pgc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class VueloApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VueloApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
