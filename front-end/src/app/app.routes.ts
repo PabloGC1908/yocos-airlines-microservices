1de5dbae8cd7872352eaf763e0508dc0e411f4df
Pablo Guerra Camana <pguerracamana@gmail.com>
2024-08-29 16:43:32 -0500

    Terminado navbar y enrutamiento

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {VuelosPageComponent} from "./pages/vuelos-page/vuelos-page.component";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
+import {NosotrosPageComponent} from "./pages/nosotros-page/nosotros-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'vuelos',
+    component: VuelosPageComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosPageComponent
+  }
+];
