4a4c9c61d63d933043d59ff5b8151f0c43fc767a
Pablo Guerra Camana <pguerracamana@gmail.com>
2024-08-29 19:26:42 -0500

    Agregando libreria Angular Material

--- front-end/src/app/pages/login-page/login-page.component.ts
+++ front-end/src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import {FormsModule} from "@angular/forms";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    FormsModule,
+    MatSlideToggle
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  email = '';
+  password = '';
 
+  mostrarCredencialesConsola() {
+    console.log(this.email);
+  }
 }
